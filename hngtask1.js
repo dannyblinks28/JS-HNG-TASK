e0752a660d8a9a46214266281126484e0b951152
dannyblinks28 <47160307+dannyblinks28@users.noreply.github.com>
2020-03-26 17:01:54 +0100

    my js task

---
+++ hngtask1.js
@@ -0,0 +1,12 @@
+let myName = "Nsirim Daniel Azeru";
+console.log(myName);
+var courses = ["HTML", "CSS", "JS"];
+console.log(courses);
+const data = [];
+for ( let i=1; i<=200; i++) {
+    if (i%2===1) {
+      data.push(i);
+    }
+}
+
+console.log(data);
